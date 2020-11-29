e8cc221fbcd34c18eac79fa0bdf9f44e26ed699e
ZhengTed <zhengted@163.com>
2020-11-29 22:03:52 +0800

    Mooc Chapter 6 practice

---
+++ GolangStudy/MoocPractice/go.mod
@@ -0,0 +1,3 @@
+module MoocPractice
+
+go 1.13
---
+++ GolangStudy/MoocPractice/retriever/mock/mockRetriever.go
@@ -0,0 +1,14 @@
+package mock
+
+type Retriever struct {
+	Contents string
+}
+
+func (r *Retriever) Post(url string, form map[string]string) string {
+	r.Contents = form["contents"]
+	return "ok"
+}
+
+func (r Retriever) Get(url string) string {
+	return r.Contents
+}
