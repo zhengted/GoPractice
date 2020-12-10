cd57d82010b4aaecdf4bc88c6a337e9e65e97960
zhengted <zhengted@163.com>
2020-12-11 00:15:12 +0800

    Add ItemSaver.go

--- crawler/persist/ItemSaver.go
+++ crawler/persist/ItemSaver.go
@@ -1,6 +1,10 @@
 package persist
 
-import "log"
+import (
+	"context"
+	"github.com/olivere/elastic/v7"
+	"log"
+)
 
 func ItemSaver() chan interface{} {
 	out := make(chan interface{})
@@ -11,7 +15,31 @@ func ItemSaver() chan interface{} {
 			log.Printf("ItemSaver got item "+
 				"#%d:%v", itemCount, item)
 			itemCount++
+
+			id, err := save(item)
+			if err != nil {
+				log.Printf("Item Saver: error saving item %v %s\n", id, err.Error())
+				continue
+			}
+
 		}
 	}()
 	return out
+}
+
+func save(item interface{}) (id string, err error) {
+	client, err := elastic.NewClient(
+		// Must turn off sniff in docker
+		elastic.SetSniff(false))
+	if err != nil {
+		return "", err
+	}
+	resp, err := client.Index().Index("dating_profile").
+		Type("zhenai").
+		BodyJson(item).
+		Do(context.Background())
+	if err != nil {
+		return "", nil
+	}
+	return resp.Id, nil
 }
--- go.mod
+++ go.mod
@@ -4,8 +4,9 @@ go 1.13
 
 require (
 	github.com/gin-gonic/gin v1.6.3
+	github.com/olivere/elastic/v7 v7.0.22
 	go.uber.org/zap v1.16.0
-	golang.org/x/net v0.0.0-20190620200207-3b0461eec859
+	golang.org/x/net v0.0.0-20200202094626-16171245cfb2
 	golang.org/x/sys v0.0.0-20200930185726-fdedc70b468f // indirect
-	golang.org/x/text v0.3.2
+	golang.org/x/text v0.3.3
 )
