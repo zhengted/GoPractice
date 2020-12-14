773c8314a3236ee9a9ed299f4363af947a7acdc9
zhengted <zhengted@163.com>
2020-12-14 12:25:24 +0800

    add grpc demo

--- go.mod
+++ go.mod
@@ -4,9 +4,13 @@ go 1.13
 
 require (
 	github.com/gin-gonic/gin v1.6.3
+	github.com/golang/protobuf v1.4.3
 	github.com/olivere/elastic/v7 v7.0.22
 	go.uber.org/zap v1.16.0
-	golang.org/x/net v0.0.0-20200202094626-16171245cfb2
-	golang.org/x/sys v0.0.0-20200930185726-fdedc70b468f // indirect
-	golang.org/x/text v0.3.3
+	golang.org/x/net v0.0.0-20201209123823-ac852fbbde11
+	golang.org/x/sys v0.0.0-20201211090839-8ad439b19e0f // indirect
+	golang.org/x/text v0.3.4
+	google.golang.org/genproto v0.0.0-20201211151036-40ec1c210f7a // indirect
+	google.golang.org/grpc v1.34.0
+	google.golang.org/protobuf v1.25.0
 )
---
+++ lang/grpc/server/server.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "GoPractice/lang/grpc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	port = ":50051"
+)
+
+type server struct{} //服务对象
+
+// SayHello 实现服务的接口 在proto中定义的所有服务都是接口
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer() //起一个服务
+	pb.RegisterGreeterServer(s, &server{})
+	// 注册反射服务 这个服务是CLI使用的 跟服务本身没有关系
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
